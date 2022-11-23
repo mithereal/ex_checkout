b8281cdbc62f6d3d0c961b2d2ea08fee93959cc8
mithereal <mithereal@gmail.com>
2022-11-23 01:23:38 -0700

    add module loader

--- lib/ex_checkout.ex
+++ lib/ex_checkout.ex
@@ -15,4 +15,29 @@ defmodule ExCheckout do
   def init do
     :ok
   end
+
+
+  @doc """
+  Returns the name and module tuple.
+
+      iex> ExCheckout.get_modules()
+      {%{}, :module_name}
+  """
+  def get_modules() do
+    {:ok, modules} = :application.get_key(:ex_checkout, :modules)
+
+    modules
+    |> Stream.map(&Module.split/1)
+    |> Stream.filter(fn module ->
+      case module do
+        [_, "Checkout", _] -> true
+        _ -> false
+      end
+    end)
+    |> Stream.map(&Module.concat/1)
+    |> Stream.map(&{&1, apply(&1, :module_id, [])})
+    |> Enum.map(fn output ->
+      output
+    end)
+  end
 end
