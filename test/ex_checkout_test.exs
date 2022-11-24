e16139c6412c38d02bbcfcbfb29c93fbd786b012
mithereal <mithereal@gmail.com>
2022-11-23 23:15:14 -0700

    make tests pass

--- test/ex_checkout_test.exs
+++ test/ex_checkout_test.exs
@@ -8,19 +8,25 @@ defmodule ExCheckoutTest do
   end
 
   test "checkout process" do
-    {:ok, pid} = ExCheckout.new()
     customer = %{first_name: "mithereal", last_name: "nil", email: "mithereal@gmail.com", phone: "1234567"}
     items = [{"sku-123", 11.00}, {"sku-123-456", 15.00}]
+    adjustments = []
+    transaction = []
+
+    {:ok, pid} = ExCheckout.new()
+
     Checkout.customer(pid, customer)
     Checkout.items(pid, items)
-    Checkout.adjustments(pid, [])
+    Checkout.adjustments(pid, adjustments)
     Checkout.scan_items(pid)
     Checkout.subtotal(pid)
-    Checkout.apply_adjustments(pid)
+
+    _adjustments_result = Checkout.apply_adjustments(pid)
     total = Checkout.total(pid)
     _invoice = Checkout.invoice(pid)
-    _state = Checkout.transaction(pid, [])
+    _state = Checkout.transaction(pid, transaction)
     _receipt = Checkout.receipt(pid)
+
     assert total == 0
   end
 end
